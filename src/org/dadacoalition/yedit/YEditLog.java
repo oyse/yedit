7385d501625b19b9f0df5982c448edc2f4080d67
oysteto@ifi.uio.no <oysteto@ifi.uio.no@ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6>
2009-04-09 18:54:06 +0000

    Added support for configuring the plugin tracing through the standard Eclipse tracing mechanism.
    git-svn-id: https://yedit.googlecode.com/svn/trunk@25 ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6

--- src/org/dadacoalition/yedit/YEditLog.java
+++ src/org/dadacoalition/yedit/YEditLog.java
@@ -1,11 +1,13 @@
 package org.dadacoalition.yedit;
 
+import java.util.HashMap;
 import java.util.logging.*;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Status;
 
 /**
- * Utility class for logging exceptions and for tracing application
+ * Utility class for logging exceptions and for tracing plugin
  * execution.
  * 
  * @author oysteto
@@ -15,9 +17,55 @@ public class YEditLog {
 	
 	public static Logger logger;
 	
+	//the trace level that is used if tracing/debugging is enabled and trace level not specified
+	public static String DEFAULT_TRACE_LEVEL = "INFO";
+	
+	public static String TRACE_LEVEL_OPTION = Activator.PLUGIN_ID + "/debug/trace_level";
+	
+	public static HashMap<String,Level> VALID_LEVELS = new HashMap<String,Level>();
+	
+	static {
+		VALID_LEVELS.put("FINEST", Level.FINEST );
+		VALID_LEVELS.put("FINER", Level.FINER );
+		VALID_LEVELS.put("FINE", Level.FINE );
+		VALID_LEVELS.put("CONFIG", Level.CONFIG );
+		VALID_LEVELS.put("INFO", Level.INFO );
+		VALID_LEVELS.put("WARNING", Level.WARNING );
+		VALID_LEVELS.put("SEVERE", Level.SEVERE );
+	}
+		
+	/**
+	 * Initialize the trace logger.
+	 * 
+	 * The trace logger can be configured by changing the tracing options
+	 * when running Eclipse.
+	 */
 	public static void initializeTraceLogger() {
+
 		logger = Logger.getLogger(Activator.PLUGIN_ID);
-		logger.setLevel(Level.INFO);
+
+		//when not running in debug mode do not perform any tracing. Errors will still
+		//be logged with the standard Eclipse error logging mechanism.
+		if( !Activator.getDefault().isDebugging() ) {
+			logger.setLevel(Level.OFF);
+			return;
+		}
+			
+		String traceLevelString = Platform.getDebugOption(TRACE_LEVEL_OPTION);
+		if( null == traceLevelString ){
+			traceLevelString = DEFAULT_TRACE_LEVEL;
+		}
+		traceLevelString = traceLevelString.toUpperCase();
+		
+		Level traceLevel;
+		if( !VALID_LEVELS.containsKey(traceLevelString) ){
+			logError( "The traceLevel '" + traceLevelString + "' is not valid. Trace level set to SEVERE" );
+			traceLevel = Level.SEVERE;
+		} else {
+			traceLevel = VALID_LEVELS.get( traceLevelString );
+		}	
+		
+		logger.setLevel(traceLevel);
 	}
 	
 	public static void logException( Throwable exception ){
@@ -37,9 +85,6 @@ public class YEditLog {
 		Activator.getDefault().getLog().log(status);
 				
 	}
-	
-
-	
 	
 
 }
