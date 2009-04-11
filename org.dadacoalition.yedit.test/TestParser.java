c792cff40dd17e0faa9f8ab19d86bbdf63189f6b
oysteto@ifi.uio.no <oysteto@ifi.uio.no@ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6>
2009-04-11 12:50:19 +0000

    Initial import of the test plugin.
    git-svn-id: https://yedit.googlecode.com/svn/trunk@36 ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6

---
+++ org.dadacoalition.yedit.test/TestParser.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.StringReader;
+import java.util.List;
+import java.util.Scanner;
+
+import org.yaml.snakeyaml.*;
+import org.yaml.snakeyaml.nodes.*;
+
+
+public class TestParser {
+
+
+	
+	public static void main( String[] args ) throws FileNotFoundException {
+		
+		Loader yamlParser = new Loader();	
+		String content = readFile( "examples/test2.yaml" );
+		
+		for( Node document : yamlParser.loadAll2( new StringReader(content) ) ){
+			dumpNode( document, 0 );
+		}
+		
+		
+	}
+	
+	public static void dumpNode( Node node, int depth ){
+		
+		int startLine = node.getStartMark().getLine();
+		int startColumn = node.getStartMark().getColumn();
+		int endLine = node.getEndMark().getLine();
+		int endColumn = node.getEndMark().getColumn();
+		
+//		for( int i = 0; i < depth; i++ ){
+//			System.out.print( " " );
+//		}
+		
+		if( node instanceof ScalarNode ){
+//			System.out.print( node.getValue() );
+		} else if ( node instanceof MappingNode ){
+			MappingNode mNode = (MappingNode) node;
+			List<Node[]> value = mNode.getValue();
+			for( Node[] child : value ){
+				dumpNode( child[0], depth + 1);
+				dumpNode( child[1], depth + 1);
+			}
+			
+		} else if( node instanceof SequenceNode ){
+			SequenceNode sNode = (SequenceNode) node;
+	        List<Node> value = sNode.getValue();
+	        for (Node child : value) {
+	            dumpNode( child, depth + 1);
+	        }
+		} else {
+//			System.out.print( "Else: " + node.getValue() );
+		}
+		
+//		System.out.println( "(" + startLine + ", " + startColumn + ", " + endLine + ", " + endColumn + ")" );
+		System.out.println( node );
+	}
+	
+	
+	public static String readFile( String filename ) throws FileNotFoundException {
+		
+		Scanner scanner = new Scanner( new File(filename) );
+		String content = "";
+		while( scanner.hasNextLine() ){
+			content += scanner.nextLine() + "\n";
+		}
+		
+		return content;		
+	}
+	
+}
