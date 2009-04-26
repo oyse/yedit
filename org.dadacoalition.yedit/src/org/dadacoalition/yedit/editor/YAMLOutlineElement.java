5f438059e3b65388340b6a550f27f702bd210dea
oysteto@ifi.uio.no <oysteto@ifi.uio.no@ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6>
2009-04-26 17:30:21 +0000

    Added Javadoc documentation.
    git-svn-id: https://yedit.googlecode.com/svn/trunk@69 ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6

--- org.dadacoalition.yedit/src/org/dadacoalition/yedit/editor/YAMLOutlineElement.java
+++ org.dadacoalition.yedit/src/org/dadacoalition/yedit/editor/YAMLOutlineElement.java
@@ -3,6 +3,7 @@ package org.dadacoalition.yedit.editor;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.dadacoalition.yedit.YEditLog;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.BadPositionCategoryException;
 import org.eclipse.jface.text.IDocument;
@@ -12,12 +13,26 @@ import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.nodes.ScalarNode;
 import org.yaml.snakeyaml.nodes.SequenceNode;
 
+/**
+ * This class is used to represent the elements in the outline view.
+ * @author oysteto
+ *
+ */
 public class YAMLOutlineElement {
 		
+        /** The element is a single scalar. */
 		public static final int SCALAR = 1;
+		
+		/** The element is part of a mapping */
 		public static final int MAPPINGITEM = 2;
+		
+		/** The element is part of a sequence */
 		public static final int SEQUENCEITEM = 3;
+		
+		/** The element is a document element */
 		public static final int DOCUMENT = 4;
+		
+		/** The element is a key value pair where the value is a single scalar */
 		public static final int MAPPINGSCALAR = 5;
 	
 		protected Node node;
@@ -29,28 +44,47 @@ public class YAMLOutlineElement {
 		protected IDocument document;
 		protected int type;
 		
+		/**
+		 * Create an DOCUMENT element. The document elements are the roots in the
+		 * outline tree
+		 * @param node A node as returned by SnakeYAML.
+		 * @param document The document containing the YAML text
+		 */
 		protected YAMLOutlineElement( Node node, IDocument document ){
 			this( node, null, DOCUMENT, document );		
 		}		
 		
+		/**
+		 * @param node A node as returned by SnakeYAML
+		 * @param parent The parent element
+		 * @param type The type of YAML element that this is. The different element types are defined
+		 * as constants in this class.
+		 * @param document The document containing the YAML text.
+		 */
 		protected YAMLOutlineElement( Node node, YAMLOutlineElement parent, int type, IDocument document ){
 			this.node = node;
 			this.parent = parent;
 			this.type = type;
 			this.document = document;
 			
-			parseData( node );
+			parseNode( node );
 			position = getPosition( node );
 			try {
 				document.addPosition(YAMLContentOutlinePage.YAMLSEGMENT, position);
 			} catch (BadLocationException e) {
-				e.printStackTrace();
+			    YEditLog.logger.warning(e.toString());				
 			} catch (BadPositionCategoryException e) {
-				e.printStackTrace();
+				YEditLog.logger.warning(e.toString());
 			}			
-
 		}
 		
+		/**
+		 * Get the position of the node within the document containing the YAML text.
+		 * This position is necessary to associate clicking on the element in the
+		 * outline view with moving the caret to the right position in the document.
+		 * @param node The SnakeYAML node of the current element.
+		 * @return
+		 */
 		private Position getPosition( Node node ){
 			
 			int startLine = node.getStartMark().getLine();
@@ -69,16 +103,25 @@ public class YAMLOutlineElement {
 					length = endColumn - startColumn;
 				}
 				p = new Position( offset, length ); 
-			} catch( BadLocationException ex) {
-				
+			} catch( BadLocationException e) {
+				YEditLog.logger.warning( e.toString() );
 			}
 			return p; 			
 			
 		}
 		
-		private void parseData( Node node ){
+		/**
+		 * Parse the node to determine what type of element it is and if
+		 * it has any children. This method recursively constructs any children of
+		 * the element as well.
+		 * @param node The SnakeYAML of the current element.
+		 */
+		private void parseNode( Node node ){
 			
 			if( node instanceof ScalarNode ){
+			    
+			    //At this stage we know that the element has no children, therefore change the
+			    //type of the element.
 				if( this.type == SEQUENCEITEM ){
 					this.type = SCALAR;
 				} else if( this.type == MAPPINGITEM ){
