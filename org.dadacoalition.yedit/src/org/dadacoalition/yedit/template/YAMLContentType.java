5561f2a940f688fd7578b919939aa09e5e719082
oysteto@ifi.uio.no <oysteto@ifi.uio.no@ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6>
2009-07-08 08:37:52 +0000

    Added support for the standard Eclipse template variables.
    git-svn-id: https://yedit.googlecode.com/svn/trunk@117 ef3a9a2a-22d2-11de-96a1-5542d1fc8aa6

--- org.dadacoalition.yedit/src/org/dadacoalition/yedit/template/YAMLContentType.java
+++ org.dadacoalition.yedit/src/org/dadacoalition/yedit/template/YAMLContentType.java
@@ -1,9 +1,25 @@
 package org.dadacoalition.yedit.template;
 
+import org.eclipse.jface.text.templates.GlobalTemplateVariables;
 import org.eclipse.jface.text.templates.TemplateContextType;
 
 public class YAMLContentType extends TemplateContextType {
     
     public static final String YAML_CONTENT_TYPE = "org.dadacoalition.yedit.template.yaml";
 
+    public YAMLContentType(){
+        addGlobalResolvers();
+    }
+    
+    private void addGlobalResolvers() {
+        addResolver(new GlobalTemplateVariables.Cursor());
+        addResolver(new GlobalTemplateVariables.WordSelection());
+        addResolver(new GlobalTemplateVariables.LineSelection());
+        addResolver(new GlobalTemplateVariables.Dollar());
+        addResolver(new GlobalTemplateVariables.Date());
+        addResolver(new GlobalTemplateVariables.Year());
+        addResolver(new GlobalTemplateVariables.Time());
+        addResolver(new GlobalTemplateVariables.User());
+    }    
+    
 }
