a67ead562969961538c66cffdd18850e5f7a6271
Øystein Torget <oyseoyse@gmail.com>
2013-01-01 19:44:03 +0100

    Added a DocumentStartAndEndRule for matching the start and end of YAML documents since this is much more efficient than using a regex on larger documents.

---
+++ org.dadacoalition.yedit/src/org/dadacoalition/yedit/editor/DocumentStartAndEndRule.java
@@ -0,0 +1,51 @@
+package org.dadacoalition.yedit.editor;
+
+import org.eclipse.jface.text.rules.ICharacterScanner;
+import org.eclipse.jface.text.rules.IRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.Token;
+
+/**
+ * Scanner rule to match the start and end of the document.
+ */
+public class DocumentStartAndEndRule implements IRule {
+
+	private IToken token;
+	
+	private String matchString;
+	
+	/**
+	 * @param matchString The string to match. Should either be '---' or '...'
+	 * @param token The token that should be returned for a match.
+	 */
+	public DocumentStartAndEndRule(String matchString, IToken token){
+		this.token = token;
+		this.matchString = matchString;
+	}
+	
+	@Override
+	public IToken evaluate(ICharacterScanner scanner) {
+
+        char c = (char) scanner.read();
+        int count = 1;
+
+        String chars = "";
+        while( c != ICharacterScanner.EOF && count < 3 ){                    
+        	chars += c;
+
+        	c = (char) scanner.read();
+            count++;            
+        }
+        
+        if( matchString.equals(chars)){
+        	return token;
+        } else {
+        	//no match so need to rewind scanner.
+        	for( int i = 0; i < count; i++ ){
+        		scanner.unread();
+        	}
+        	return Token.UNDEFINED;
+        }
+	}
+
+}
--- org.dadacoalition.yedit/src/org/dadacoalition/yedit/editor/YAMLScanner.java
+++ org.dadacoalition.yedit/src/org/dadacoalition/yedit/editor/YAMLScanner.java
@@ -104,8 +104,8 @@ public class YAMLScanner extends BufferedRuleBasedScanner {
         rules.add(new EndOfLineRule("#", commentToken));
         rules.add(new EndOfLineRule("%TAG", directiveToken));
         rules.add(new EndOfLineRule("%YAML", directiveToken));
-        rules.add(new RegexRule("---", documentStartToken));
-        rules.add(new RegexRule("\\.\\.\\.", documentEndToken));
+        rules.add(new DocumentStartAndEndRule("---", documentStartToken));
+        rules.add(new DocumentStartAndEndRule("...", documentEndToken));
         rules.add(new IndicatorCharacterRule(indicatorCharToken));
         rules.add(new WhitespaceRule(whitespaceToken));
         rules.add(new WordPatternRule(new AnchorWordDetector(), "&", "", anchorToken));
