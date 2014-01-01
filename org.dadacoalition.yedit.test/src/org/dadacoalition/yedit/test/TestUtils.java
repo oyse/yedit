d1cb8c502d15bcf8f5103ebd0038441c626e7cd2
Øystein Torget <oyseoyse@gmail.com>
2014-01-01 15:02:45 +0100

    Replaced custom made file to string reader with Apache utils to get more consistent handling of end-of-line.

--- org.dadacoalition.yedit.test/src/org/dadacoalition/yedit/test/TestUtils.java
+++ org.dadacoalition.yedit.test/src/org/dadacoalition/yedit/test/TestUtils.java
@@ -1,26 +1,25 @@
 package org.dadacoalition.yedit.test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+
+import org.apache.commons.io.IOUtils;
 
 public class TestUtils {
 
 	public static String readFile( String filename ) {
 		
-		Scanner scanner;
-		try {
-			scanner = new Scanner( new File(filename) );
-		} catch (FileNotFoundException e) {		
-			throw new RuntimeException(e);
-		}
-		
-		String content = "";
-		while( scanner.hasNextLine() ){
-			content += scanner.nextLine() + "\n";
-		}
-		
-		return content;		
+	    try {
+            FileInputStream is = new FileInputStream(filename);
+            return IOUtils.toString(is);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 	}	
 	
 }
