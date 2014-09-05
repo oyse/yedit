92f7a8e3b3573fbda6249b039dc882628015c938
Øystein Torget <oyseoyse@gmail.com>
2014-09-05 20:20:01 +0200

    Replaced utility method for reading of file content with IOUtils method instead

--- org.dadacoalition.yedit.test/src/org/dadacoalition/yedit/test/TestUtils.java
+++ org.dadacoalition.yedit.test/src/org/dadacoalition/yedit/test/TestUtils.java
@@ -1,26 +1,23 @@
 package org.dadacoalition.yedit.test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
 
 public class TestUtils {
 
 	public static String readFile( String filename ) {
 		
-		Scanner scanner;
-		try {
-			scanner = new Scanner( new File(filename) );
-		} catch (FileNotFoundException e) {		
-			throw new RuntimeException(e);
-		}
-		
-		String content = "";
-		while( scanner.hasNextLine() ){
-			content += scanner.nextLine() + "\n";
-		}
-		
-		return content;		
+	    try {
+            return IOUtils.toString(new FileInputStream(new File(filename)));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 	}	
 	
 }
